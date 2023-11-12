83f9c919e0dff002a767a4f9a43183a26e95c749
Lornavav <>
2023-11-12 15:32:09 +0000

    Customize admin panal

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Collectable, Platform, Genre
 
-admin.site.register(Collectable)
-admin.site.register(Platform)
-admin.site.register(Genre)
+
+class CollectableAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'platform',
+        'genre',
+        'year',
+        'price'
+    )
+
+    ordering = ('name',)
+
+class PlatformAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
+admin.site.register(Collectable, CollectableAdmin)
+admin.site.register(Platform, PlatformAdmin)
+admin.site.register(Genre, GenreAdmin)
