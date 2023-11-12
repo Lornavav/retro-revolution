1f833f03974a3be3163bbedab23f7e805a8a1676
Lornavav <>
2023-11-12 15:54:14 +0000

    Add products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Collectable
 
-# Create your views here.
+
+def all_collectables(request):
+    """ A view to show all products, including sorting and search queries"""
+
+    collectables = Collectable.objects.all()
+
+    context = {
+        'collectables': collectables,
+    }
+
+    return render(request, 'collectables/collectables.html', context)
