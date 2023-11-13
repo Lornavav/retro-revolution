62f25ba0d02f81e6e313408e7b7a4c77dbca7155
Lornavav <>
2023-11-13 23:20:56 +0000

    Add detail template, view and url for collectables

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.all_collectables, name='collectables')
+    path('', views.all_collectables, name='collectables'),
+    path('<int:collectable_id>/', views.collectable_detail, name='collectable_detail'),
 ]
-
--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Collectable
 
 
 def all_collectables(request):
-    """ A view to show all products, including sorting and search queries"""
+    """ A view to show all collectables, including sorting and search queries"""
 
     collectables = Collectable.objects.all()
 
@@ -12,3 +12,15 @@ def all_collectables(request):
     }
 
     return render(request, 'collectables/collectables.html', context)
+
+
+def collectable_detail(request, collectable_id):
+    """ A view to show more detail on a single collectable"""
+
+    collectable = get_object_or_404(Collectable, pk=collectable_id)
+
+    context = {
+        'collectable': collectable,
+    }
+
+    return render(request, 'collectables/collectable_detail.html', context)
