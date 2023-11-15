33129028455059680cf2dc1a3e4263ac83d7b0e7
Lornavav <>
2023-11-15 22:03:08 +0000

    Start adding search functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Collectable
 
 
@@ -6,9 +8,21 @@ def all_collectables(request):
     """ A view to show all collectables, including sorting and search queries"""
 
     collectables = Collectable.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('collectables'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            collectables = collectables.filter(queries)
 
     context = {
         'collectables': collectables,
+        'search_term': query,
     }
 
     return render(request, 'collectables/collectables.html', context)
