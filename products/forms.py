58833e6839e821166b81c0c91c5439e428062977
Lornavav <lornavavasour@yahoo.ie>
2023-12-03 00:16:03 +0000

    Add product form for admin management

---
+++ products/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Collectable, Platform
+
+
+class CollectableForm(forms.ModelForm):
+
+    class Meta:
+        model = Collectable
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        platforms = Platform.objects.all()
+
+        self.fields['category'].choices = name
+        for field_name, field in self.fields.items():
+            field.widget.attr['class'] = 'border-black rounded-0'
+
+
